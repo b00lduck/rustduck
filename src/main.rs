f98e060cb389eb9a6e35df19b3284cfffbf7ffcd
Daniel Zerlett <zerlett@network-innovation.de>
2019-04-20 20:19:58 +0200

    three kubes

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 extern crate kiss3d;
 extern crate nalgebra as na;
 
-use na::{Vector3, UnitQuaternion};
+use na::{Vector3, UnitQuaternion, Translation3};
 use kiss3d::window::Window;
 use kiss3d::light::Light;
 use kiss3d::scene::SceneNode;
@@ -9,11 +9,13 @@ use kiss3d::scene::SceneNode;
 fn main() {
     let mut window = Window::new("Kiss3d: wireframe");
 
-    let mut cs: [SceneNode; 4] = [
+    let mut cs: [SceneNode; 6] = [
         cube(&mut window, 0, 0),
         cube(&mut window, 1, 0),
         cube(&mut window, 0, 1),
-        cube(&mut window, 1, 1)
+        cube(&mut window, 1, 1),
+        cube(&mut window, 0, 2),
+        cube(&mut window, 1, 2)
     ];
 
     window.set_light(Light::StickToCamera);
@@ -30,6 +32,7 @@ fn main() {
 fn cube(window: &mut Window, n: i32, x: i32) -> SceneNode {
 
     let mut c = window.add_cube(1.0, 1.0, 1.0);
+    let transe = Translation3::new((x*2) as f32, 0.0f32, 0.0f32);
 
     if n == 0 {
         c.set_color(0.0, 1.0, 0.5);
@@ -39,6 +42,8 @@ fn cube(window: &mut Window, n: i32, x: i32) -> SceneNode {
     } else {
         c.set_color(0.7, 0.2, 0.1);
     }
+
+    c.set_local_translation(transe);
 
     return c
 }
