148abbec0839767709442c3e0cf819c11a353a94
Daniel Zerlett <zerlett@network-innovation.de>
2019-04-21 23:30:25 +0200

    timestamp for rendering

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 extern crate kiss3d;
 extern crate nalgebra as na;
 
+use kiss3d::post_processing::SobelEdgeHighlight;
 //use na::{Vector3, UnitQuaternion, Point3};
 use kiss3d::window::Window;
 use kiss3d::light::Light;
 //use kiss3d::camera::FirstPerson;
+use std::time::{SystemTime, UNIX_EPOCH};
 
 mod cubeblock;
 mod target;
@@ -21,16 +23,39 @@ fn main() {
     
     let mut maingroup = window.add_group();
 
+    let mut sobel = SobelEdgeHighlight::new(4.0);
+
     //let mut node = target::add_nodeGroup(&mut maingroup);
     let mut node = target::Node::init_node(&mut maingroup, 0,0,0,1);
 
     playfield::add_playfield(&mut maingroup, &map);
 
     window.set_light(Light::StickToCamera);
+    window.set_framerate_limit(Some(60));
 
-    while window.render() { 
-        eventhandler::move_event(&mut window, &mut node);
+    let mut frames_rendered = 0usize;
+  
+    let mut now = SystemTime::now();
+
+    while !window.should_close() {
+        
+        match now.elapsed() {
+            Ok(elapsed) => {                
+                let time_gone = elapsed.as_micros();
+                println!("{}", time_gone);
+                now = SystemTime::now();
+                // move(time_gone)
+                window.render_with_effect(&mut sobel);
+                frames_rendered = frames_rendered + 1;               
+            }
+            Err(e) => {
+                println!("Error: {:?}", e);
+            }
+        }
+
     }
+ 
+
   
 }
 
--- src/playfield.rs
+++ src/playfield.rs
@@ -11,9 +11,9 @@ pub fn add_playfield(scene_node: &mut SceneNode, map: &Map) -> SceneNode {
     for x in 0..map.size.x {
         for y in 0..map.size.y {                      
             if map.state[y as usize][x as usize] == 0 {
-                add_cube(&mut g1, x - map.size.x/2, y - map.size.y/2, -4);
+                add_cube(&mut g1, x - map.size.x/2, y - map.size.y/2, 0);
             }
-        }
+        } 
     }    
 
     return g1
@@ -21,7 +21,7 @@ pub fn add_playfield(scene_node: &mut SceneNode, map: &Map) -> SceneNode {
 
 fn add_cube(scene_node: &mut SceneNode, x: i16, y: i16, z: i16) -> SceneNode {
 
-    let mut c = scene_node.add_cube(1.0, 1.0, 1.0);
+    let mut c = scene_node.add_cube(1.0, 1.0, 4.0);
     let t1 = Translation3::new((x) as f32, (y) as f32, (z) as f32);
 
     c.set_local_translation(t1);
